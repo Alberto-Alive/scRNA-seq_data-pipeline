fbcaeab18005b8c2de889f3c47437c2538db5dc6
BitSurgeon <alberto-alive@outlook.com>
2026-06-25 22:51:25 +0300

    auto-commit

--- src/scpipe/pipeline.py
+++ src/scpipe/pipeline.py
@@ -24,3 +24,17 @@ def run_pipeline(adata: AnnData, cfg: PipelineConfig) -> AnnData:
     logger.info("QC: %d cells in", adata.n_obs)
     adata = qc.apply_qc_filters(adata, cfg.qc)
     logger.info("QC: %d cells kept", adata.n_obs)
+
+    adata = preprocess.preprocess(adata, cfg.preprocess)
+    adata = integrate.integrate(adata, cfg.integrate)
+    adata = cluster.cluster(adata, cfg.cluster)
+    adata = annotate.annotate(adata, cfg.annotate)
+
+    metrics = evaluate.integration_metrics(
+        adata, batch_key=cfg.integrate.batch_key, label_key="cell_type"
+    )
+
+    adata.uns["integration_metrics"] = metrics
+    logger.info("Integrationmetrics: %s", metrics)
+
+    return adata
