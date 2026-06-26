01692964c8f7ed2a6c61fa277808d1b713e4eb51
BitSurgeon <alberto-alive@outlook.com>
2026-06-26 05:10:06 +0300

    auto-commit

--- src/scpipe/qc.py
+++ src/scpipe/qc.py
@@ -9,7 +9,7 @@ from scpipe.config import QCConfig
 
 def annotate_qc_metrics(adata: AnnData,  mito_prefix: str = "MT-") -> AnnData:
     adata.var["mt"] = adata.var_names.str.upper().str.startswith(mito_prefix.upper())
-    sc.pp.claculate_qc_metrics(adata, qc_vars=["mt"], percent_top=None, log1p=False, inplace=True)
+    sc.pp.calculate_qc_metrics(adata, qc_vars=["mt"], percent_top=None, log1p=False, inplace=True)
     return adata
 
 
