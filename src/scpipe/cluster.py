d4c459a1987b2ff65b910690c0af4db351b3632a
BitSurgeon <alberto-alive@outlook.com>
2026-06-26 04:55:14 +0300

    auto-commit

--- src/scpipe/cluster.py
+++ src/scpipe/cluster.py
@@ -10,4 +10,16 @@ from scpipe.integrate import REP_KEY
 
 
 def cluster(adata: AnnData, cfg: ClusterConfig ) -> AnnData:
-    rep = REP_KEY if 
+    rep = REP_KEY if REP_KEY in data.obsm else "X_pca"
+    sc.pp.neighbors(adata, n_neighbors=cfg.n_neighbors, use_repo=rep, random_state=cfg.random_state)
+    sc.tl.leiden(
+        adata,
+        resolution=cfg.resolution,
+        random_state=cfg.random_state,
+        flavor="igraph",
+        n_iterations=2,
+        directed=False
+    )
+
+    sc.tl.umap(adata, random_state=cfg.random_state)
+    return adata
